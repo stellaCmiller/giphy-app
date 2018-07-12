e049f8b882722d077d925013484ec38e4583e5af
stellaCmiller <stella.miller@utexas.edu>
2018-07-12 15:41:14 -0500

    Basic requirements met

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
+//Contains all the methods necessary to searching the giphy api and displaying gifs
 var giphyApp = {
+    apiKey: "6WQSswR7NKx3IHsKier7FfZ0jOJ0Eqbi",
     gifTopics: ["cats", "dogs", "chris pratt", "batman", "birds", "Michael Scott", "birthday"],
 
     makeButtons: function(){
@@ -14,16 +16,53 @@ var giphyApp = {
         this.gifTopics.push(userInput);
     },
 
-    searchTopic: function(query){
-        
+    searchAndDisplay: function(query){
+        queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`;
+        $.get(queryURL).then(function(response){
+            console.log(response);
+            response.data.forEach(e =>{
+            $("#gif-display").append(`<div class="gif-box"><img 
+            class="gif" 
+            animated="${e.images.fixed_height.url}" 
+            still ="${e.images.fixed_height_still.url}" 
+            state= "still" 
+            src="${e.images.fixed_height_still.url}">
+            <p>Rating: ${e.rating}</p>
+            <p>Favorite this Gif? </p>
+            </div>`);
+            });
+        });
+    },
+
+    toggleAnimation: function(gif){
+        if ($(gif).attr("state") == "still"){
+            $(gif).attr("src", `${$(gif).attr("animated")}`);
+            $(gif).attr("state", "animated");
+        } else {
+            $(gif).attr("src", `${$(gif).attr("still")}`);
+            $(gif).attr("state", "still");
+        }
     }
 }
 
+//called initially to display starting list of gif topics buttons
 giphyApp.makeButtons();
 
+//When the user enters a gif topic and presses enter or clicks 'enter', the topic is added to the topic array and a button is created for it
 $("#add-topic").click( function() {
     event.preventDefault();
     giphyApp.addTopic($("#user-input").val());
     giphyApp.makeButtons();
     $("#user-input").val("");
+})
+
+//When the user clicks a gif button, the gifphy API is queried and 10 gifs are displayed
+$("body").on("click", ".gif-button", function(){
+    $("#gif-display").empty();
+    giphyApp.searchAndDisplay($(this).text());
+})
+
+//When a still gif is clicked, it animates. If an animated gif is click, it stops
+$("body").on("click", ".gif", function(){
+    giphyApp.toggleAnimation(this);
 })
